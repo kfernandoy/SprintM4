c8c7cab2d4ffd6a84d19311cbefe10cb7980e39d
kfernandoy <k.fernandoy@gmail.com>
2024-09-23 22:21:24 -0300

    Sprint Módulo 4

--- app/src/main/java/com/sprintm4/MainActivity.java
+++ app/src/main/java/com/sprintm4/MainActivity.java
@@ -1,24 +1,38 @@
 package com.sprintm4;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageProfile;
+    private Button buttonGoToProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        imageProfile = findViewById(R.id.imageProfile);
+        buttonGoToProfile = findViewById(R.id.buttonGoToProfile);
+
+        // Glide Pic
+        Glide.with(this)
+                .load("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Anonymous_emblem.svg/320px-Anonymous_emblem.svg.png") // Reemplaza con la URL de tu imagen
+                .placeholder(R.drawable.profile_placeholder) // Imagen de placeholder
+                .error(R.drawable.profile_error) // Imagen en caso de error
+                .into(imageProfile);
+
+        // ProfileActivity Button
+        buttonGoToProfile.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+            startActivity(intent);
         });
     }
 }
--- app/src/main/java/com/sprintm4/ProfileActivity.java
+++ app/src/main/java/com/sprintm4/ProfileActivity.java
@@ -1,24 +1,60 @@
 package com.sprintm4;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button buttonLinkedIn;
+    private Button buttonWhatsApp;
+    private Button buttonEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        buttonLinkedIn = findViewById(R.id.buttonLinkedIn);
+        buttonWhatsApp = findViewById(R.id.buttonWhatsApp);
+        buttonEmail = findViewById(R.id.buttonEmail);
+
+        // LinkedIn
+        buttonLinkedIn.setOnClickListener(v -> {
+            Intent linkedInIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/kfernandoy/"));
+            startActivity(linkedInIntent);
+        });
+
+        // WhatsApp
+        buttonWhatsApp.setOnClickListener(v -> {
+            String phoneNumberWithCountryCode = "56945953996";  // Phone number
+            String message = "Hello world!";  // Default message
+
+            Intent whatsappIntent = new Intent(Intent.ACTION_VIEW);
+            whatsappIntent.setData(Uri.parse("https://api.whatsapp.com/send?phone=" + phoneNumberWithCountryCode + "&text=" + Uri.encode(message)));
+
+            try {
+                // Try opening WhatsApp
+                startActivity(whatsappIntent);
+            } catch (Exception e) {
+                e.printStackTrace();
+                // If WhatsApp not installed go to browser instead
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://api.whatsapp.com/send?phone=" + phoneNumberWithCountryCode + "&text=" + Uri.encode(message)));
+                startActivity(browserIntent);
+            }
+        });
+
+        // Email
+        buttonEmail.setOnClickListener(v -> {
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:k.fernandoy@gmail.com"));
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Asunto del mensaje");
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Cuerpo del mensaje");
+            startActivity(emailIntent);
         });
     }
 }
+
